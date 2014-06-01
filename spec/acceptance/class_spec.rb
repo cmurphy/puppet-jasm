c9b25766584d12c2fe4f8e7c878112bb2491d973
Colleen Murphy <cmurphy@cat.pdx.edu>
2014-06-01 12:46:30 -0700

    Testing

---
+++ spec/acceptance/class_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper_acceptance'
+
+describe 'jasm class' do
+
+  context 'default parameters' do
+    # Using puppet_apply as a helper
+    it 'should work with no errors' do
+      pp = <<-EOS
+      class { 'jasm': }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_failures => true)
+    end
+
+    describe package('openssh-server') do
+      it { should be_installed }
+    end
+
+    describe service('ssh') do
+      it { should be_running }
+    end
+  end
+end
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,5 +3,6 @@ describe 'jasm' do
 
   context 'with defaults for all parameters' do
     it { should contain_class('jasm') }
+    it { should contain_file('/etc/ssh/sshd_config') }
   end
 end
