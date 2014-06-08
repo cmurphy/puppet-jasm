053d1be83fb3fbab6a644cda4ed84b52788cb3dc
Colleen Murphy <cmurphy@cat.pdx.edu>
2014-06-08 13:46:49 -0700

    Add unit tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,8 +1,25 @@
 require 'spec_helper'
 describe 'jasm' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('jasm') }
-    it { should contain_file('/etc/ssh/sshd_config') }
+  describe "on osfamily RedHat" do
+    let(:facts) {{ :osfamily => 'RedHat' }}
+
+    context 'with defaults for all parameters' do
+      it { should contain_class('jasm') }
+      it { should contain_file('/etc/ssh/sshd_config') }
+      it { should contain_package('openssh-server') }
+      it { should contain_service('sshd') }
+    end
+  end
+
+  describe "on osfamily Debian" do
+    let(:facts) {{ :osfamily => 'Debian' }}
+
+    context 'with defaults for all parameters' do
+      it { should contain_class('jasm') }
+      it { should contain_file('/etc/ssh/sshd_config') }
+      it { should contain_package('openssh-server') }
+      it { should contain_service('ssh') }
+    end
   end
 end
